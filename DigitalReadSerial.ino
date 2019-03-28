c10762fef1e955eb44be2f46f1263791d3f8cc39
Johnnie5DubV <jmered01@rams.shepherd.edu>
2019-03-28 10:28:29 -0400

    format and bug checking

--- DigitalReadSerial.ino
+++ DigitalReadSerial.ino
@@ -1,32 +1,39 @@
 #include <Servo.h>
 // 
-int leftDistSPint, rightDistSPin, frontDistSPin, poten, fanPin, pingSPin, bottomLineSPin, servoLeftPos, servoRightPos;
-int fireSPin = 3;
-Servo servoLeft, servoRight;
-int stop = 90;
+int leftDistancePin = A2;
+int rightDistancePin = A0;
+int frontDistancePin = A1;
+int bottomLineSPin = A3;
+int poten, fanPin, pingSPin;
+int firePin = A4;
+Servo servoLeft;
+Servo servoRight;
 int fireState;
 int fLeft= 180;
 int fRight= 0;
-int threshold = 200;              // distance threshold for sensors to tell how to navigate
+int threshold = 700;              // distance threshold for sensors to tell how to navigate
 int valLeft = 0;                  // variable to store the values from LEFT sensor(initially zero)
 int valRight = 0;                 // variable to store the values from RIGHT sensor(initially zero)
 int valForward = 0;               // variable to store the values from FORWARD sensor(initially zero)
 int onLine = 0;
-//right distance has min 132 max 768
-//front distance mins 132 andmax 767
-//left distance min 112 and max 767
-//line sensor min 16 and max is 1023
-//fire has max of 985
+//right distance has min 132 max 768 pin A0
+//front distance mins 132 andmax 767 pin A1
+//left distance min 112 and max 767 pin A2
+//line sensor min 16 and max is 1023 pin a 3
+//fire has max of 985 pin a4 and a5
+
 
 // the setup routine runs once when you press reset:
-void setup() {
+void setup() 
+{
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
   pinMode(fanPin, OUTPUT);
+ 
   //setup movement servos
-  servoLeft.attach();
-  servoRight.attach();
+  servoLeft.attach(6);
+  servoRight.attach(3);
   servoLeft.write(90);
   servoRight.write(90);
 }
@@ -36,15 +43,15 @@ void setup() {
 void loop() 
 {
   // check for fire sensor active:
-  readSensor()// possibly want a read sensor method that takes a reading of all sensors readSensor()
-  
+  readSensors();// possibly want a read sensor method that takes a reading of all sensors readSensor()
+  leftHandWallFollow();
   delay(1);        // delay in between reads for stability
   
   if(bottomLineSPin>50 && fireState){
     //stop moving
       
     //aim
-    digitalWrite(fanPin);
+    digitalWrite(fanPin, 145);
     
   }
   
@@ -53,40 +60,9 @@ void loop()
     leftHandWallFollow();
   }
     
-  
-  void leftHandWallFollow()
-  {
-    readSensor();
-    
-    if(valLeft > threshold && valRight > threshold && valFront < threshold) //2 way intersection
-    {
-      turnLeft();
-    }
-    
-    else if(valLeft < threshold && valRight < threshold && (valFront > threshold) ) // empty space
-    { 
-        forward();//Moves straight                                                                                      
-    }
-    
-    else if(valLeft < threshold && valRight < threshold && valForward < threshold)
-    {
-      backup();
-      turnaround();
-    }
-    
-    else if(valLeft > threshold) 
-    {
-      turnLeft(); // ???need to add amount of time to turn for???
-    }
-    
-    else if(valLeft < threshold && valFront <threshold && valRight > threshold)
-    {
-      turnRight();
-    }
-    
-  }
-  
-  void stop()
+}
+
+void stopMoving()
   {
     servoLeft.write(90);
     servoRight.write(90);
@@ -99,18 +75,18 @@ void loop()
   }
   void backward()
   {
-    servoLeft.write(leftBackward);
-    servoRight.write(rightBackward);
+    servoLeft.write(fRight);
+    servoRight.write(fLeft);
   }
   void turnLeft()
   {
-    servoLeft.write(stop);
-    servoRight.write(rightForward);
+    servoLeft.write(90);
+    servoRight.write(fRight);
   }
   void turnRight()
   {
-    servoLeft.write(fLeft));
-    servoRight.write(stop);
+    servoLeft.write(fLeft);
+    servoRight.write(90);
   }
   void turnAround()
   {
@@ -119,13 +95,60 @@ void loop()
   }
   void readSensors()
   {
-    valLeft = digitalRead(leftDistancePin);
-    valRight = digitalRead(rightDistancePin);
-    valFront = digitalRead(frontDistancePin);
-    fireState = digitalRead(firePin);
+    valLeft = analogRead(leftDistancePin);
+    valRight = analogRead(rightDistancePin);
+    valForward = analogRead(frontDistancePin);
+    fireState = analogRead(firePin);
+    Serial.println("vleft " + valLeft);
+    Serial.println("vright " + valRight);
+    Serial.println("vforward " +  valForward);
+    Serial.println("fs " + fireState);
+    
+  }
+  void aim()
+  {
+    
+    
   }
   
-}
+  void averageSensor()
+  {
+
+  }
+  void leftHandWallFollow()
+  {
+    readSensors();
+    
+    if(valLeft > threshold && valRight > threshold && valForward < threshold) //2 way intersection
+    {
+      turnLeft();
+    }
+    
+    else if(valLeft < threshold && valRight < threshold && (valForward > threshold) ) // empty space
+    { 
+        forward();//Moves straight                                                                                      
+    }
+    
+    else if(valLeft < threshold && valRight < threshold && valForward < threshold)
+    {
+      backward();
+      turnAround();
+    }
+    
+    else if(valLeft > threshold) 
+    {
+      turnLeft(); // ???need to add amount of time to turn for???
+    }
+    
+    else if(valLeft < threshold && valForward <threshold && valRight > threshold)
+    {
+      turnRight();
+    }
+    
+  }
+  
+  
+  
 
 
 
